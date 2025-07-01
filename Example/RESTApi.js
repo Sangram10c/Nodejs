60e1ee8e093f8ac166d2dbe991afd3de043eee14
Sangram10c <chougulesangram3gmail.com>
2025-07-01 15:43:07 +0530

    Added Middleware

--- Example/RESTApi.js
+++ Example/RESTApi.js
@@ -9,50 +9,63 @@ app.use(express.json());
 // Middleware to serve static files from the 'public' directory
 const fs = require('fs');
 
-app.use((res, req, next) => {
+app.use((req, res, next) => {
     console.log("Hello Sanam");
+    next();
+});
 
-    next()
-})
+
 //middleware
 app.use((req, res, next) => {
     fs.appendFile("data.txt", `\n ${Date.now()}: ${req.ip} ${req.method} ${req.path}`,
         (err, data) => {
-            // res.send("Hello Sanam");
-            next()
+            next();
         })
-})
+});
 
 
 app.get("/api/users", (req, res) => {
-
     const html =
         `<ul>
         ${users.map((user) =>
             `<li>${user.first_name} ${user.last_name} :  ${user.Job_title}</li>`
         ).join('')}
         </ul>`
-    res.header("X-Author","Sangram");
+
+
+    res.header("X-Author", "Sangram");// Custom header (Always use X prefix for custom headers)
     res.send(html);
 });
+
 
 app.route("/api/users/:id").get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
     return res.json(user);
 
 });
+
 // app.put((req, res) => {
 //     return res.json({ status: "Pending" })
 
 // });
+
+
 app.post("/api/users", (req, res) => {
     const body = req.body;
     users.push({ ...body, id: users.length + 1 });
     fs.writeFileSync('./MOCK_DATA.json', JSON.stringify(users, null, 2));
-    return res.json({ status: "Success", id: users.length });
+    if (!body.first_name || !body.last_name || !body.Job_title || !body.email || !body.gender) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+    return res.status(201).json({ status: "User Created Successfully", id: users.length });
 
 });
+
+
 app.delete("/api/users/:id", (req, res) => {
     const body = req.body;
     users.splice(users.findIndex((user) => user.id === Number(req.params.id)), 1);
@@ -60,6 +73,8 @@ app.delete("/api/users/:id", (req, res) => {
     return res.json({ status: "Success", id: req.params.id });
 
 });
+
+
 app.patch("/api/users/:id", (req, res) => {
     const id = Number(req.params.id);
     const body = req.body;
