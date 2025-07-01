725ea12fbe93e25ec6b47bc705a27d2109fb91c8
Sangram10c <chougulesangram3gmail.com>
2025-07-01 17:31:16 +0530

    Connect to mongodb

---
+++ Model/data.js
@@ -0,0 +1,31 @@
+const { timeStamp } = require('console');
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    Job_title: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.set('timestamps', true);// Automatically adds createdAt and updatedAt fields
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
